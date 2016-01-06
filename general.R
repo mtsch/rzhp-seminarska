42b4252e23a6305ec11c67d9e6fa8a1ae49caa9b
mtsch <matijacufar@gmail.com>
2016-01-06 13:42:02 +0100

    popravek, imena v matriki, funkcija za plot

--- general.R
+++ general.R
@@ -1,14 +1,14 @@
-library(reshape2)
-
 # readFile : filename -> adjacency matrix
 readFile <- function(filename)
 {
+  require(reshape2)
+
   # read
   tbl  <- read.table(filename, sep=" ", stringsAsFactors=F)
 
   # make sure columns 2 and 3 have the same factor levels
-  lvls   <- union(tbl[, 2], tbl[, 3])
-  people <- lapply(tbl[, 2:3], factor, levels=lvls)
+  names  <- union(tbl[, 2], tbl[, 3])
+  people <- lapply(tbl[, 2:3], factor, levels=names)
 
   # numeric values of preferences
   pref <- as.character(tbl[, 1])
@@ -23,16 +23,31 @@ readFile <- function(filename)
   A <- acast(tbl, V2 ~ V3, value.var = "pref", fill=0)
 
   # make the graph undirected
-  A + t(A)
+  diag(A) <- NA
+
+  A = A + t(A)
+
+  colnames(A) <- names
+  rownames(A) <- names
+
+  A
 }
 
 # scoreFun : permutation, adjacency matrix -> score
 scoreFun <- function(perm, A)
 {
-  n    <- length(perm)
-  from <- perm[1:(n - 1)]
-  to   <- to  [2:n]
+  n <- length(perm)
 
   sum(diag(A[ perm[1:(n - 1)]
             , perm[2:n] ]))
+}
+
+# plotGraph : adjacency matrix -> plot
+plotGraph <- function(A, ...)
+{
+  require(igraph)
+
+  g <- graph_from_adjacency_matrix(A, weighted=T, mode="undirected", diag=F)
+
+  plot(g, edge.label=E(g)$weight, ...)
 }
