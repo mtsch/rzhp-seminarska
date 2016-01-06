aca69c7bb55b01e8b10323eb81e3adcf05e10fe7
mtsch <matijacufar@gmail.com>
2016-01-06 15:38:49 +0100

    popravljen scoreFun

--- general.R
+++ general.R
@@ -23,23 +23,27 @@ readFile <- function(filename)
   A <- acast(tbl, V2 ~ V3, value.var = "pref", fill=0)
 
   # make the graph undirected
-  diag(A) <- NA
-
   A = A + t(A)
 
   colnames(A) <- names
   rownames(A) <- names
 
+  A[lower.tri(A, diag=T)] <- 0
+
   A
 }
 
-# scoreFun : permutation, adjacency matrix -> score
-scoreFun <- function(perm, A)
+# scoreFun : upper.tri of adjacency matrix, permutation -> score
+scoreFun <- function(A, perm)
 {
-  n <- length(perm)
+  n <- nrow(A)
 
-  sum(diag(A[ perm[1:(n - 1)]
-            , perm[2:n] ]))
+  # row indices according to permutation
+  Ar <- matrix(rep(perm, n), nrow=n)
+
+  # distance between two students is equal to:
+  #   col.index - row.index - 1 * -weight
+  sum(((t(Ar) - Ar - 1) * -A))
 }
 
 # plotGraph : adjacency matrix -> plot
@@ -47,19 +51,21 @@ plotGraph <- function(A, ...)
 {
   require(igraph)
 
-  n <- nrow(A)
 
   # create graph, make sure weights of 0 are treated as edges
   g <- graph_from_adjacency_matrix( A + .1
                                   , weighted=T
-                                  , mode="undirected"
+                                  , mode="upper"
                                   , diag=F)
+
   E(g)$weight <- E(g)$weight - .1
   weights     <- E(g)$weight
 
   edge.cols <- c("red", "red", "gray", "green", "green")[weights + 3]
-  layoutFun <- function(i) matrix(c( sin(2*pi*1:n / n)
-                                   , cos(2*pi*1:n / n)), ncol=2)
+
+  layoutrng <- 2*pi*1:nrow(A) / nrow(A)
+  layoutFun <- function(i) matrix(c( sin(layoutrng)
+                                   , cos(layoutrng)), ncol=2)
 
   plot( g
       , edge.label   = weights
