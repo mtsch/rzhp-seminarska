5e71d1142c1236e0d7ec26ac41db827e2324bbd5
mtsch <matijacufar@gmail.com>
2016-01-16 18:13:47 +0100

    memetic

--- MA.R
+++ MA.R
@@ -1,27 +1,48 @@
 source("general.R")
 source("vnsearch.R")
 
-
 MA <- function(adjmat,                   # Adjacency matrix
                score         = scoreFun, # Score function
-               improveFun    = identity, # Additional optimization
+               selectFun     = rouletteWheelSelect2,
+               improveFun    = id,       # Additional optimization
                crossFun      = orderedX, # Crossover function
                mutateFun     = mutate,   # Mutation function
-               mutation.rate = 0.1,      # Probability of mutation
+               mutation.rate = 0.01,     # Probability of mutation
                init.pop.size = 10000,    # Initial population size
-               n.iter        = 1000,     # Number of iterations
-               n.pool        = 100       # Size of the breeding pool
+               n.iter        = 100,      # Number of iterations
+               n.pool        = 100,      # Size of the breeding pool
+               verbose       = T,        # Verbose?
+               ...                       # Arguments to improveFun
                )
 {
-    n   <- nrow(adjmat)
+    n    <- nrow(adjmat)
+    # Overall best
+    best.p <- NA
+    best.f <- 0
     # Initial population - inidividuals are placed in columns.
-    pop <- sapply(1:pop.size, function(.) sample(n))
+    pop  <- sapply(1:init.pop.size, function(.) sample(n))
 
     for (i in 1:n.iter) {
         # Get fitnesses
         fitness  <- apply(pop, 2, score, A=adjmat)
+
+        # Current best
+        c.best.f <- max(fitness)
+        if (c.best.f > best.f) {
+            best.p <- pop[, which.max(fitness)]
+            best.f <- c.best.f
+        }
+
+        # Print status
+        if (verbose) {
+            message(paste0("Iteration ", i, ":"))
+            message(c.best.f)
+        }
+
         # Select breeding pool
-        pool     <- ruletteWheelSelect(pop, fitness, n.pool)
+        pool     <- selectFun(pop, fitness, n.pool)
+        # Apply optimization
+        pool     <- apply(pool, 2, improveFun, adjmat, score, ...)
 
         # Select individuals to breed
         breed1   <- sample(n.pool, n.pool, replace=T)
@@ -44,8 +65,14 @@ MA <- function(adjmat,                   # Adjacency matrix
     # select best
     fitness <- apply(pop, 2, score, A=adjmat)
 
-    pop[, which.max(fitness)]
+    c.best.f <- max(fitness)
+    if (c.best.f > best.f) {
+        best.p <- pop[, which.max(fitness)]
+        best.f <- c.best.f
+    }
 
+    message(paste0("Final score: ", best.f, "."))
+    best.p
 }
 
 # Check if perm is a valid permutation.
@@ -70,10 +97,20 @@ mutate <- function(perm, mutation.rate)
 }
 
 # Select n individuals from pop
-# ruletteWheelSelect : population, fitness vector, num → population
-ruletteWheelSelect <- function(pop, fitness, n)
+# rouletteWheelSelect : population, fitness vector, num → population
+rouletteWheelSelect1 <- function(pop, fitness, n)
 {
     fitness <- order(fitness)
+
+    vs <- runif(n, min=0, max=sum(fitness))
+    is <- findInterval(vs, cumsum(fitness), rightmost.closed=T) + 1
+
+    pop[, is]
+}
+
+rouletteWheelSelect2 <- function(pop, fitness, n)
+{
+    fitness <- fitness - min(fitness)
 
     vs <- runif(n, min=0, max=sum(fitness))
     is <- findInterval(vs, cumsum(fitness), rightmost.closed=T) + 1
@@ -108,3 +145,17 @@ orderedX <- function(perm1, perm2)
     list(child1[!is.na(child1)],
          child2[!is.na(child2)])
 }
+
+# Improve the permutation by performing a single step.
+singleStepLocalSearch <- function(perm, adj, score, neigh)
+{
+    neighs  <- t(neigh(perm))
+    fitness <- apply(neighs, 2, score, A=adj)
+    if (max(fitness) > score(adj, perm))
+        neighs[, which.max(fitness)]
+    else
+        perm
+}
+
+# Identity function that takes more than one argument
+id <- function(x, ...) x
